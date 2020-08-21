2184b6789315f652936418ae2a7e672adcee0de8
jiyer2 <jiyer2@nmdp.org>
2020-08-21 12:58:21 -0500

    A basic Angular UI has been added for the tarr2fhir converter
    This will be dockerized in future iteration
    
    Signed-off-by: jiyer2 <jiyer2@nmdp.org>

--- src/main/java/org/nmdp/tarr2fhir/Tarr2fhirApplication.java
+++ src/main/java/org/nmdp/tarr2fhir/Tarr2fhirApplication.java
@@ -3,10 +3,12 @@ package org.nmdp.tarr2fhir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableWebMvc
 @ComponentScan({"io.swagger.api", "org.nmdp"})
 public class Tarr2fhirApplication {
 
